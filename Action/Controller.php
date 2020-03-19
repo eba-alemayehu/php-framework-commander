92fad61e5b2ef3862109edc88c5e56e5bcfe2a65
eba <ebaaleamyhu3@gmail.com>
2020-03-20 01:31:08 +0300

    templete generator function created

--- Action/Controller.php
+++ Action/Controller.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace Commander\Action; 
+namespace Commander\Action;
+
+use Exception;
+
 require 'Action.php'; 
 
 class Controller extends Action {
@@ -12,15 +15,13 @@ class Controller extends Action {
     }
 
     public function run(array $params){
-        $controller_templet = fread(fopen(APPLICATION_ROOT."/vendor/Application/Commander/Action/Templets/controller.php", "r"),
-        filesize(APPLICATION_ROOT."/vendor/Application/Commander/Action/Templets/controller.php"));
         if(isset($params[0])){
-            $controller_templet = str_replace("{name}", $params[0], $controller_templet);
+            $this->templet(
+                __DIR__."/Templets/controller.php", 
+                APPLICATION_ROOT."/app/Http/Controllers/".$params[0]."Controller.php", 
+                ["name" => $params[0]]); 
         }else{
-            die("module name and controller name is not supplied"); 
+            throw new Exception("Controller name is not supplied"); 
         }
-
-        $new_controller = fopen(APPLICATION_ROOT."/app/Http/Controllers/".$params[0]."Controller.php", "w+");
-        fwrite($new_controller, $controller_templet); 
     }
 }
